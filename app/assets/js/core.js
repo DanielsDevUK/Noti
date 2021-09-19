454abb4bb98dd37b47ac8028b34300c2699630f8
Daniels Dev <67105408+DanielsDevUK@users.noreply.github.com>
2021-09-19 22:14:02 +0100

    First Commit
    Made simple styling and a little bit of CSS for testing

---
+++ app/assets/js/core.js
@@ -0,0 +1,9 @@
+$('#folder').click( function(){
+    $('#Menu-Folder').toggleClass('disable')
+    $('#Menu-Folders').toggleClass('disable')
+})
+
+$('#back').click( function(){
+    $('#Menu-Folder').toggleClass('disable')
+    $('#Menu-Folders').toggleClass('disable')
+})
